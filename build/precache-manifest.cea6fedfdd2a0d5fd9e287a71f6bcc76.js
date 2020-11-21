434455a279a03d8721dab952f637a1317926d22f
razik <razik@gmail.com>
2020-11-21 20:47:38 +0530

    first commit

---
+++ build/precache-manifest.cea6fedfdd2a0d5fd9e287a71f6bcc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda222d80d1dfa296e6fa2297abf336e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b049603bb5f083e0b18a",
+    "url": "/static/css/main.2e051d66.chunk.css"
+  },
+  {
+    "revision": "7d9065a736f782816e2d",
+    "url": "/static/js/2.a6ed427a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6ed427a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b049603bb5f083e0b18a",
+    "url": "/static/js/main.6cd0c97e.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
